0da7ef926cc0a8f65986c16995e5447cb528f5e0
Daniel Tiago Fischer <danieltgfischer.@gmail.com>
2021-05-16 17:19:34 -0300

    refactor: changed reducer state with immer

--- src/store/module/cart/reducer.ts
+++ src/store/module/cart/reducer.ts
@@ -1,16 +1,22 @@
 import { Reducer } from 'redux';
+import produce from 'immer';
 import { ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
   items: [],
 }
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART':
-      return { ...state, items: [...state.items, { product: action.payload.product, quantity: 1 }] }
-    default:
-      return state;
-  }
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+        draft.items.push({ product, quantity: 1 })
+        break;
+      }
+      default:
+        return draft;
+    }
+  })
 }
 
 export default cart;
