5d55d171ad6ceb2c221a50dfa2363fb1eb0dc288
Daniel Tiago Fischer <danieltgfischer.@gmail.com>
2021-05-16 18:17:29 -0300

    chore: add redux devtool library and configure it

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { ICartState } from './module/cart/types';
 import rootReducer from './module/rootReducer';
 export interface IState {
   cart: ICartState;
 }
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store;
